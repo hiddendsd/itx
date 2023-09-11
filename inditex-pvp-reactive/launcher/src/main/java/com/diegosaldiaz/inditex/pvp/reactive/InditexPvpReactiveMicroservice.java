bd3adbf3a9a411d978df244022f46987136d37c7
Diego Sal Díaz <diego.sal@ext.leroymerlin.es>
2023-09-11 17:54:09 +0200

    chore(reactive): Initial structure

---
+++ inditex-pvp-reactive/launcher/src/main/java/com/diegosaldiaz/inditex/pvp/reactive/InditexPvpReactiveMicroservice.java
@@ -0,0 +1,11 @@
+package com.diegosaldiaz.inditex.pvp.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexPvpReactiveMicroservice {
+    public static void main( String[] args ) {
+        SpringApplication.run(InditexPvpReactiveMicroservice.class, args);
+    }
+}
---
+++ inditex-pvp-reactive/launcher/src/main/resources/db/migrations/common/V1.0.0__initial_structure.sql
@@ -0,0 +1,15 @@
+create table if not exists price
+(
+    id            integer generated always as identity constraint price_pk primary key,
+    brand_id      integer not null,
+    start_date    timestamp not null,
+    end_date      timestamp not null,
+    price_list    integer not null,
+    product_id    integer not null,
+    priority      integer not null,
+    price         numeric(20,2) not null,
+    currency      varchar(3)
+);
+
+create index if not exists price_product_index on price (product_id);
+
