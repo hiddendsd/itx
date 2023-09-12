4669405b5ff45709be52330665447ae96bdd419d
Diego Sal Díaz <diego.sal@ext.leroymerlin.es>
2023-09-12 16:49:23 +0200

    style(servlet): library versions moved to properties

--- inditex-pvp-servlet/launcher/src/it/java/com/diegosaldiaz/inditex/pvp/infrastructure/outbound/h2/repository/PriceRepositoryIT.java
+++ inditex-pvp-servlet/launcher/src/it/java/com/diegosaldiaz/inditex/pvp/infrastructure/outbound/h2/repository/PriceRepositoryIT.java
@@ -2,19 +2,11 @@ package com.diegosaldiaz.inditex.pvp.infrastructure.outbound.h2.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import com.diegosaldiaz.inditex.pvp.BaseIT;
-import com.diegosaldiaz.inditex.pvp.InditexPvpServletMicroservice;
-import com.diegosaldiaz.inditex.pvp.infrastructure.outbound.h2.repository.PriceRepository;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 
 class PriceRepositoryIT extends BaseIT {
   private static final int BRAND_ID = 1;
